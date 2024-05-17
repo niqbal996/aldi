6138256b960d802a7fc667ce5bccd4edb2e54ac9
Naeem Iqbal <naeemiqbal996@gmail.com>
2024-05-17 10:29:01 +0200

    Fixed paths

--- tools/scripts/aldi_train.sh
+++ tools/scripts/aldi_train.sh
@@ -4,4 +4,4 @@ srun -K --ntasks=1 --gpus-per-task=1 -N 1 --cpus-per-gpu=20 -p H100 --mem=50000 
   --mail-type=end --mail-user=naeem.iqbal@dfki.de --job-name=aldi_uda \
   --container-workdir=/home/iqbal/aldi \
   --time=1-00:00 \
-  bash uda.sh
+  bash tools/scripts/uda.sh
--- tools/scripts/baseline.sh
+++ tools/scripts/baseline.sh
@@ -1,2 +1,2 @@
 #!/usr/bin/env bash
-python3 tools/train_net.py --config configs/cityscapes/Base-RCNN-VitDetB-Phenobench_strongaug_ema.yaml
+python3 tools/train_net.py --config configs/phenobench/Base-RCNN-VitDetB-Phenobench_strongaug_ema.yaml
